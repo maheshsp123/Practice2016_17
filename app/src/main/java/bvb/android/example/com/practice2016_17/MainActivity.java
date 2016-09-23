82964568c0d6241883f85002d9c09f2a1b79916d
mahesh <maheshsp123@gmail.com>
2016-09-23 15:11:18 +0530

    commit 23sep2017 college

--- app/src/main/java/bvb/android/example/com/practice2016_17/MainActivity.java
+++ app/src/main/java/bvb/android/example/com/practice2016_17/MainActivity.java
@@ -2,15 +2,9 @@ package bvb.android.example.com.practice2016_17;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     //AppCompatActivity thisActivityReference;
     Button firstActBtnRefernce;
     AppCompatActivity thisActivityReference;
+    //test line college pc
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
