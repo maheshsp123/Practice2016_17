f402bea2a0a7ae04274ef8a7865b65df0554a328
mahesh <maheshsp123@gmail.com>
2016-09-26 13:03:51 +0530

    commit 26sep2017 college LP minor1 qno-1C

---
+++ app/src/main/java/bvb/android/example/com/practice2016_17/lp_minor1_1c_Activity.java
@@ -0,0 +1,13 @@
+package bvb.android.example.com.practice2016_17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lp_minor1_1c_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lp_minor1_1c);
+    }
+}
